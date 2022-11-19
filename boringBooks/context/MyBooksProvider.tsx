16dd623ddfb62f13ae040e620e5890213ca4a966
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 09:59:00 +0200

    Rokas

--- boringBooks/context/MyBooksProvider.tsx
+++ boringBooks/context/MyBooksProvider.tsx
@@ -21,13 +21,17 @@ const MyBooksProvider = ({ children }: Props) => {
     const [savedBooks, setSavedBooks] = useState<Book[]>([]);
 
     const isBookSaved = (book: Book) => {
-
+        return savedBooks.some((savedBook) => JSON.stringify(savedBook) === JSON.stringify(book));
     }
 
     const onToggleSaved = (book: Book) => {
-
+        if (isBookSaved(book)) {
+            // This filters out the book from the savedBooks array
+            setSavedBooks((books) => books.filter((item) => item !== book));
+        } else {
+            setSavedBooks((books) => [book, ...books]);
+        }
     }
-
 
     return (
         // @ts-ignore
