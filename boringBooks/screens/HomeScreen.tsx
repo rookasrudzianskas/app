84f2f8768d9dbab9559d1ce6bc453d64771278ad
Rokas <rookas.rudzenskas@gmail.com>
2022-11-19 09:25:15 +0200

    Rokas

--- boringBooks/screens/HomeScreen.tsx
+++ boringBooks/screens/HomeScreen.tsx
@@ -44,6 +44,24 @@ const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
     const [provider, setProvider] = useState<"googleBooksSearch" | "openLibrarySearch">('googleBooksSearch');
     const [runQuery, { data, loading, error }] = useLazyQuery(query);
 
+    const parseBook = (item): Book => {
+        if (provider === "googleBooksSearch") {
+            return {
+                title: item?.volumeInfo?.title,
+                image: item?.volumeInfo?.imageLinks?.thumbnail,
+                authors: item?.volumeInfo?.authors,
+                isbn: item?.volumeInfo?.industryIdentifiers?.[0]?.identifier,
+            };
+        } else {
+            return {
+                title: item?.title,
+                authors: item.author_name,
+                image: `https://covers.openlibrary.org/b/olid/${item.cover_edition_key}-M.jpg`,
+                isbn: item.isbn?.[0],
+            }
+        }
+    }
+
     if(loading) {
         return (
             <View className="h-screen items-center justify-center">
@@ -111,11 +129,7 @@ const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
                   keyExtractor={(item, index) => `${index}-${item.id}`}
                   showsVerticalScrollIndicator={false}
                   renderItem={({ item, index }) => (
-                      <BookItem book={{
-                          title: item?.volumeInfo?.title,
-                          image: item?.volumeInfo?.imageLinks?.thumbnail,
-                          authors: item?.volumeInfo?.authors,
-                      }} />
+                      <BookItem book={parseBook(item)} />
                   )}
               />
           )}
